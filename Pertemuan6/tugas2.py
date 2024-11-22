ea58e6b21fa8ba35f31ce33753ac4c98f56cfcb4
yuri <yahsyayurida@gmail.com>
2024-11-22 11:57:43 +0700

    Add files via upload

---
+++ Pertemuan6/tugas2.py
@@ -0,0 +1,16 @@
+#Buat lah output dari menggunakan bahasa pemprograman python dengan : 
+#1 + 3 + 5 + 7 +9 +11 +13 + 15 +17 +19 = ….
+
+print("")
+print("2.Buat lah output dari menggunakan bahasa pemprograman python dengan : 1 + 3 + 5 + 7 +9 +11 +13 + 15 +17 +19 = …")
+print("")
+
+jumlah_total_bil_ganjil=0
+bilangan_ganjil=1
+
+while bilangan_ganjil <=19:
+    jumlah_total_bil_ganjil += bilangan_ganjil
+    bilangan_ganjil +=2
+
+print("1 + 3 + 5 + 7 + 9 + 11 + 13 + 15 +17 +19 =", jumlah_total_bil_ganjil)
+print("")
